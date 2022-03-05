a2430ddf2f46dafaec92202f1975fffba1b1c527
Dattaraj <dattaraj@nimapinfotech.com>
2022-03-05 12:17:52 +0530

    added constituencies

---
+++ api/beaches.js
@@ -0,0 +1,164 @@
+var data = [
+  {
+    "id": "1",
+    "name": "Calangute Beach",
+    "taluka":"Bardez",
+    "desc": "Calangute beach is one of most famous place in goa to visit. Lots  of  famous resturent and pubs are present here.",
+    "images": ["images/beaches/calangute_beach_goa.png"],
+    "location":"https://www.google.com/maps/dir//calangute+beach/data=!4m6!4m5!1m1!4e2!1m2!1m1!1s0x3bbfea0148f4ed2b:0xcb592fad5d257d17?sa=X&ved=2ahUKEwiH-OnpmYH2AhXASWwGHTGcB9IQ9Rd6BAgzEAQ",
+    "status": "1"
+  },
+  {
+    "id": "2",
+    "name": "Baga Beach",
+    "taluka":"Bardez",
+    "desc": "Baga Beach is another famous beach of goa, attached along with calangute beach many people visit here in large number.",
+    "images": ["images/beaches/baga_beach.png"],
+    "location":"https://www.google.com/maps/dir//Baga+beach/data=!4m6!4m5!1m1!4e2!1m2!1m1!1s0x3bbfea1cd8a1deff:0xba7066c0ee35c1e0?sa=X&ved=2ahUKEwjYxYT_3IP2AhXTrlYBHWcDC0AQ9Rd6BAgyEAQ",
+    "status": "1"
+  },
+  {
+    "id": "3",
+    "name": "Paloem Beach",
+    "taluka":"Bardez",
+    "desc": "Paloem Beach is suituated at south goa is known for its clam surrounding. Many people visit here to enjoy water sports",
+    "images": ["images/beaches/paloem_beach.png"],
+    "location":"https://www.google.com/maps/dir//Palolem+beach/data=!4m6!4m5!1m1!4e2!1m2!1m1!1s0x3bbe4551d05b02bb:0x1e1bc67d4b0fbbf5?sa=X&ved=2ahUKEwiZy92J3YP2AhUDklYBHRxdBJ0Q9Rd6BAg2EAQ",
+    "status": "1"
+  },
+  {
+    "id": "4",
+    "name": "Cab De Ram Beach",
+    "taluka":"Bardez",
+    "desc": "Cab De Ram Beach is suituated in south goa. its one of the quiet place to visit. you can also visit Cab de Ram fort present near beach",
+    "images": ["images/beaches/cab_de_ram.png"],
+    "location":"https://www.google.com/maps/dir//cabo+de+rama+beach/data=!4m6!4m5!1m1!4e2!1m2!1m1!1s0x3bbe4bfd44daf1e5:0x6678f36253fe7b44?sa=X&ved=2ahUKEwj3xIiX3YP2AhXes1YBHYb2DD8Q9Rd6BAgsEAQ",
+    "status": "1"
+  },
+  {
+    "id": "5",
+    "name": "Cola Beach",
+    "taluka":"Bardez",
+    "desc": "Cola Beach is one of the underrated and clean beach of goa.",
+    "images": ["images/beaches/cola_beach.png"],
+    "location":"https://www.google.com/maps/dir//Cola+Beach+Road,+Cola,+Goa/@15.0579197,73.9629595,15z/data=!3m1!4b1!4m10!4m9!1m1!4e2!1m5!1m1!1s0x3bbe4ee33e70345b:0x5c4bf66c7098b942!2m2!1d73.9717143!2d15.0579199!3e0",
+    "status": "1"
+  },
+  {
+    "id": "6",
+    "name": "Candolim Beach",
+    "taluka":"Bardez",
+    "desc": "Candolim Beach is famous for its resturent and pubs.",
+    "images": ["images/beaches/candolim-beach.jpg"],
+    "location":"https://www.google.com/maps/dir/15.493586,73.8301434/Candolim+Beach+Rd,+Candolim,+Goa+403515/@15.5125015,73.7666871,13z/data=!3m1!4b1!4m10!4m9!1m1!4e1!1m5!1m1!1s0x3bbfc193f5f837bb:0x75b2929e91152780!2m2!1d73.7652214!2d15.5183307!3e0",
+    "status": "1"
+  },
+  {
+    "id": "7",
+    "name": "Vagator Beach",
+    "taluka":"Bardez",
+    "desc": "Vagator Beach has great landscape.It is famous for picnic amongs local.",
+    "images": ["images/beaches/vagator1.jpg"],
+    "location":"https://www.google.com/maps/dir/15.5538051,73.7647483/vagator+beach+goa/@15.5776893,73.7319266,14z/data=!3m1!4b1!4m9!4m8!1m1!4e1!1m5!1m1!1s0x3bbfe96efd4101af:0x1fe7bea632841b03!2m2!1d73.733627!2d15.6029835",
+    "status": "1"
+  },
+  {
+    "id": "8",
+    "name": "Colva Beach",
+    "taluka":"Bardez",
+    "desc": "Colva Beach is one of the famous beach near madgoa city. Many people goes here with family to spend time.",
+    "images": ["images/beaches/colva-beach.jpg"],
+    "location":"https://www.google.com/maps/dir/15.5538051,73.7647483/colva+beach+goa/@15.4255253,73.7472516,11z/data=!3m1!4b1!4m9!4m8!1m1!4e1!1m5!1m1!1s0x3bbfb418862d72a3:0xb5de1b202cad0935!2m2!1d73.9119945!2d15.2805316",
+    "status": "1"
+  },
+  {
+    "id": "9",
+    "name": "Morjim Beach",
+    "taluka":"Bardez",
+    "desc": "Morjim Beach for people looking for clam and cool place to spend your time. It is famous for its turtle migration.",
+    "images": ["images/beaches/morjim_beach.jpg"],
+    "location":"https://www.google.com/maps/dir/15.5538051,73.7647483/morjim+beach/@15.594201,73.6957465,12z/data=!3m1!4b1!4m9!4m8!1m1!4e1!1m5!1m1!1s0x3bbfeecadf09462f:0x8d0b22824d00b999!2m2!1d73.7214261!2d15.6343609",
+    "status": "1"
+  },
+  {
+    "id": "10",
+    "name": "Arambol Beach",
+    "taluka":"Bardez",
+    "desc": "Arambol Beach is the one to spend time with family and budget outing.",
+    "images": ["images/beaches/arambol_beach.jpg"],
+    "location":"https://www.google.com/maps/dir/15.5538051,73.7647483/Arambol+Beach/@15.6151049,73.7011547,12z/data=!3m1!4b1!4m9!4m8!1m1!4e1!1m5!1m1!1s0x3bbfee52ef136653:0xdd79ca2621f10716!2m2!1d73.7032836!2d15.6846886",
+    "status": "1"
+  },
+  {
+    "id": "11",
+    "name": "Agonda Beach",
+    "taluka":"Bardez",
+    "desc": "Agonda Beach is for you to visit if you want to spot dolphin with mesmerizing sunset.",
+    "images": ["images/beaches/agonda_beach.jpg"],
+    "location":"https://www.google.com/maps/dir/15.5538051,73.7647483/Agonda,+Goa/@15.2946092,73.6198364,10z/data=!3m1!4b1!4m10!4m9!1m1!4e1!1m5!1m1!1s0x3bbe4fbb85faee3d:0xef5d75dacb8f9d45!2m2!1d73.9888797!2d15.0455814!3e0",
+    "status": "1"
+  },
+  {
+    "id": "12",
+    "name": "Majorda Beach",
+    "taluka":"Bardez",
+    "desc": "Majorda Beach is a visit place for the food loving people.",
+    "images": ["images/beaches/Majorda_Beach.jpg"],
+    "location":"https://www.google.com/maps/dir/15.3171056,73.9007086/Butterfly+Beach/@15.1700754,73.8283887,11z/data=!3m1!4b1!4m9!4m8!1m1!4e1!1m5!1m1!1s0x3bbe4f8a16e887f1:0x894249fead443527!2m2!1d74.001647!2d15.0196019",
+    "status": "1"
+  },
+  {
+    "id": "13",
+    "name": "Butterfly Beach",
+    "taluka":"Bardez",
+    "desc": "Butterfly Beach is one of the least visited place in goa But famous for advanture lovers as one need to walk for 20-30 min.",
+    "images": ["images/beaches/Butterfly_Beach.jpg"],
+    "location":"https://www.google.com/maps/dir/15.5538051,73.7647483/Majorda+Beach/@15.4324109,73.7060034,11z/data=!3m1!4b1!4m9!4m8!1m1!4e1!1m5!1m1!1s0x3bbfb6edeec5c283:0x5b9dac94fbc4ced9!2m2!1d73.9018114!2d15.3111553",
+    "status": "1"
+  },
+  {
+    "id": "14",
+    "name": "Benaulim Beach",
+    "taluka":"Bardez",
+    "desc": "Benaulim Beach is one of the peaceful beach of goa. Good for family outing",
+    "images": ["images/beaches/Benaulim_beach.jpg"],
+    "location":"https://www.google.com/maps/dir/15.3171056,73.9007086/Benaulim+Beach/@15.2896595,73.893845,13z/data=!3m1!4b1!4m9!4m8!1m1!4e1!1m5!1m1!1s0x3bbfb46c046c5e83:0x79c87dcb52764f87!2m2!1d73.9187138!2d15.2570626",
+    "status": "1"
+  },
+  {
+    "id": "15",
+    "name": "Miramar Beach",
+    "taluka":"Bardez",
+    "desc": "Miramar Beach is the most famous and visited beach of goa.",
+    "images": ["images/beaches/Miramar_Beach.jpg"],
+    "location":"https://www.google.com/maps/dir//Miramar,+Panaji,+Goa/@15.477546,73.8030183,15z/data=!4m8!4m7!1m0!1m5!1m1!1s0x3bbfc0db2f977b03:0xd609133f181e989d!2m2!1d73.8121396!2d15.4774686",
+    "status": "1"
+  },
+  {
+    "id": "16",
+    "name": "Galgibaga Beach",
+    "taluka":"Bardez",
+    "desc": "Galgibaga Beach is known for its peaceful environment. Must visit for peace loving people.",
+    "images": ["images/beaches/Galgibaga_Beach.jpg"],
+    "location":"https://www.google.com/maps/dir/15.4738363,73.8071738/Galgibaga+Beach/@15.2288748,73.6752698,10z/data=!3m1!4b1!4m9!4m8!1m1!4e1!1m5!1m1!1s0x3bbe5b3c39a8699f:0x9ec1bd1c29b6b529!2m2!1d74.0495654!2d14.9600472",
+    "status": "1"
+  },
+  {
+    "id": "17",
+    "name": "Betalbatim Beach",
+    "taluka":"Bardez",
+    "desc": "Betalbatim Beach is for people who are love to swim on beach.",
+    "images": ["images/beaches/Betalbatim_Beach.jpg"],
+    "location":"https://www.google.com/maps/dir/15.4738363,73.8071738/Betalbatim+Beach/@15.3912122,73.7689712,11z/data=!3m1!4b1!4m9!4m8!1m1!4e1!1m5!1m1!1s0x3bbfb69999d215ab:0x49ea0a569de7b862!2m2!1d73.9078744!2d15.2926227",
+    "status": "1"
+  },
+  {
+    "id": "18",
+    "name": "Rajbagh Beach",
+    "taluka":"Bardez",
+    "desc": "Rajbagh Beach is the best place to spend time with loved one.",
+    "images": ["images/beaches/Rajbagh_Beach.jpg"],
+    "location":"https://www.google.com/maps/dir//Rajbagh+Beach/data=!4m6!4m5!1m1!4e2!1m2!1m1!1s0x3bbe5ace64f6f98d:0xff93653757ee6b71?sa=X&ved=2ahUKEwiE3IrnppD2AhWyIaYKHenGAiEQ9Rd6BAgtEAQ",
+    "status": "1"
+  },
+];
---
+++ api/waterfall.js
@@ -0,0 +1,56 @@
+var data = [
+  {
+    "id": "1",
+    "name": "Dudhsagar Waterfall",
+    "taluka":"Bardez",
+    "desc": "One of the famous waterfall in the world. Suituated at the heart of mhadei wildlife sanctuary with beautiful landscape.",
+    "images": ["images/waterfalls/dudhsagar.jpg"],
+    "location":"https://www.google.com/maps/dir//Dudhsagar+Waterfall/@15.3144266,74.2442668,12z/data=!3m1!4b1!4m9!4m8!1m1!4e2!1m5!1m1!1s0x3bbfa618142b8b43:0xfd9448e7283b0225!2m2!1d74.3143073!2d15.3144375",
+    "status": "1"
+  },
+  {
+    "id": "2",
+    "name": "Baga Beach",
+    "taluka":"Bardez",
+    "desc": "Baga Beach is another famous beach of goa, attached along with calangute beach many people visit here in large number.",
+    "images": ["images/beaches/baga_beach.png"],
+    "location":"https://www.google.com/maps/dir//Baga+beach/data=!4m6!4m5!1m1!4e2!1m2!1m1!1s0x3bbfea1cd8a1deff:0xba7066c0ee35c1e0?sa=X&ved=2ahUKEwjYxYT_3IP2AhXTrlYBHWcDC0AQ9Rd6BAgyEAQ",
+    "status": "1"
+  },
+  {
+    "id": "3",
+    "name": "Paloem Beach",
+    "taluka":"Bardez",
+    "desc": "Paloem Beach is suituated at south goa is known for its clam surrounding. Many people visit here to enjoy water sports",
+    "images": ["images/beaches/paloem_beach.png"],
+    "location":"https://www.google.com/maps/dir//Palolem+beach/data=!4m6!4m5!1m1!4e2!1m2!1m1!1s0x3bbe4551d05b02bb:0x1e1bc67d4b0fbbf5?sa=X&ved=2ahUKEwiZy92J3YP2AhUDklYBHRxdBJ0Q9Rd6BAg2EAQ",
+    "status": "1"
+  },
+  {
+    "id": "4",
+    "name": "Cab De Ram Beach",
+    "taluka":"Bardez",
+    "desc": "Cab De Ram Beach is suituated in south goa. its one of the quiet place to visit. you can also visit Cab de Ram fort present near beach",
+    "images": ["images/beaches/cab_de_ram.png"],
+    "location":"https://www.google.com/maps/dir//cabo+de+rama+beach/data=!4m6!4m5!1m1!4e2!1m2!1m1!1s0x3bbe4bfd44daf1e5:0x6678f36253fe7b44?sa=X&ved=2ahUKEwj3xIiX3YP2AhXes1YBHYb2DD8Q9Rd6BAgsEAQ",
+    "status": "1"
+  },
+  {
+    "id": "5",
+    "name": "Cola Beach",
+    "taluka":"Bardez",
+    "desc": "Cola Beach is one of the underrated and clean beach of goa.",
+    "images": ["images/beaches/cola_beach.png"],
+    "location":"https://www.google.com/maps/dir//Cola+Beach+Road,+Cola,+Goa/@15.0579197,73.9629595,15z/data=!3m1!4b1!4m10!4m9!1m1!4e2!1m5!1m1!1s0x3bbe4ee33e70345b:0x5c4bf66c7098b942!2m2!1d73.9717143!2d15.0579199!3e0",
+    "status": "1"
+  },
+  {
+    "id": "6",
+    "name": "Candolim Beach",
+    "taluka":"Bardez",
+    "desc": "Candolim Beach is famous for its resturent and pubs.",
+    "images": ["images/beaches/candolim-beach.jpg"],
+    "location":"https://www.google.com/maps/dir/15.493586,73.8301434/Candolim+Beach+Rd,+Candolim,+Goa+403515/@15.5125015,73.7666871,13z/data=!3m1!4b1!4m10!4m9!1m1!4e1!1m5!1m1!1s0x3bbfc193f5f837bb:0x75b2929e91152780!2m2!1d73.7652214!2d15.5183307!3e0",
+    "status": "1"
+  },
+];
