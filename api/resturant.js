1a53fe443460a17e42ee75e519d36f092f54fa21
Dattaraj <dattaraj@nimapinfotech.com>
2022-03-06 20:38:43 +0530

    Removed sliders

---
+++ api/resturant.js
@@ -0,0 +1,92 @@
+var data = [
+  {
+    "id": "1",
+    "name": "Starlight",
+    "taluka":"Bardez",
+    "desc": "Starlight is one of most famous restaurants in south goa at Assolna village. This restaurants is famous for the seafood.",
+    "images": ["images/restaurants/starlight.jpg"],
+    "location":"https://www.google.com/maps/dir//star+light+assolna/data=!4m6!4m5!1m1!4e2!1m2!1m1!1s0x3bbe4cc6c78ba1dd:0x8b8ff44b279e2ba5?sa=X&ved=2ahUKEwi824ek34P2AhUisFYBHZDwAZkQ9Rd6BAgsEAQ",
+    "status": "1"
+  },
+  {
+    "id": "2",
+    "name": "Ritz Classic Panjim",
+    "taluka":"Bardez",
+    "desc": "Ritz Classic Panjim is another famous restaurants of goa. It is one of the famous family restaurants in goa.",
+    "images": ["images/restaurants/ritz.jpg"],
+    "location":"https://www.google.com/maps/dir//Ritz+Classic+Panjim/data=!4m6!4m5!1m1!4e2!1m2!1m1!1s0x3bbfc0892b7aac1b:0x711b75fe40723402?sa=X&ved=2ahUKEwia9uyu34P2AhXeqFYBHWaTCdIQ9Rd6BAgqEAQ",
+    "status": "1"
+  },
+  {
+    "id": "3",
+    "name": "Antique Mardol",
+    "taluka":"Bardez",
+    "desc": "Antique Mardol is family restaurants at verna, south goa. It is known for its antique goan foods",
+    "images": ["images/restaurants/royal_treat_restaurants.jfif"],
+    "location":"https://www.google.com/maps/dir//Royal+Treat+Marcela/data=!4m6!4m5!1m1!4e2!1m2!1m1!1s0x3bbfbdb56f247b59:0xa15567f27f788b46?sa=X&ved=2ahUKEwi84ZzK34P2AhXR4jgGHXBOB1gQ9Rd6BAgPEAQ",
+    "status": "1"
+  },
+  {
+    "id": "4",
+    "name": "Royal Treat Marcela",
+    "taluka":"Bardez",
+    "desc": "Royal Treat is a family restaurants at marcela, Goa. It is one of the underated but famous restaurants amongs local.",
+    "images": ["images/waterfalls/tambdisurla.jpg"],
+    "location":"https://www.google.com/maps/dir//Tambdi+Surla+Waterfall/@15.421868,74.1962086,12z/data=!3m1!4b1!4m9!4m8!1m1!4e2!1m5!1m1!1s0x3bbf088abfdfa505:0xbea61dfa873fbc3!2m2!1d74.2662491!2d15.421879",
+    "status": "1"
+  },
+  {
+    "id": "5",
+    "name": "Darya Dager",
+    "taluka":"Bardez",
+    "desc": "Darya Dager is a family bar and restaurants at old goa, famous for its seafoods.",
+    "images": ["images/restaurants/darya_dager.jpg"],
+    "location":"https://www.google.com/maps/dir//Darya+Dager/data=!4m6!4m5!1m1!4e2!1m2!1m1!1s0x3bbfbe897e5f8f7f:0xafe7767be8857468?sa=X&ved=2ahUKEwiw69Ho34P2AhWxwjgGHXEFCOwQ9Rd6BAgNEAQ",
+    "status": "1"
+  },
+  {
+    "id": "6",
+    "name": "The Fisherman’s Wharf",
+    "taluka":"Bardez",
+    "desc": "The Fisherman’s Wharf is famous for their signature fusion Goan dishes. There are three outlate at Panaji, Calangute & Cavelossim.",
+    "images": ["images/restaurants/the-fisherman-s-wharf.jpg"],
+    "location":"https://www.google.com/maps/dir/15.5538051,73.7647483/The+Fisherman's+Wharf,+H.No.+2%2F35B+%26+2%2F35C,+Naika+Waddo,+Calangute,+Goa+403516/@15.5453471,73.7590854,15z/data=!3m1!4b1!4m9!4m8!1m1!4e1!1m5!1m1!1s0x3bbfc12359f2f1d3:0x87cb280e2a74356!2m2!1d73.7664248!2d15.538489",
+    "status": "1"
+  },
+  {
+    "id": "7",
+    "name": "Martin's Corner",
+    "taluka":"Bardez",
+    "desc": "Martin's Corner is a family bar and restaurants present at Binwaddo, Salcette, Betalbatim.",
+    "images": ["images/restaurants/Martins_corner.jpg"],
+    "location":"https://www.google.com/maps/dir/15.5538051,73.7647483/Martin's+Corner/@15.4279304,73.7060034,11z/data=!3m1!4b1!4m9!4m8!1m1!4e1!1m5!1m1!1s0x3bbfb6dda67ab363:0x7be42cfd4e9de95!2m2!1d73.9143083!2d15.3037892",
+    "status": "1"
+  },
+  {
+    "id": "8",
+    "name": "Purple Martini At Sunset Point, Goa",
+    "taluka":"Bardez",
+    "desc": "Purple Martini is a bar and resturent at Anjuna Beach, famous for its sunset view seating.",
+    "images": ["images/restaurants/Purple_Martini.jpg"],
+    "location":"https://www.google.com/maps/dir//Purple+Martini+At+Sunset+Point,+Goa/data=!4m6!4m5!1m1!4e2!1m2!1m1!1s0x3bbfe9835f8284e5:0xb0e2ffdf3e551d62?sa=X&ved=2ahUKEwjX-M-cn5D2AhVvyYsBHTVcAyIQ9Rd6BAhFEAU",
+    "status": "1"
+  },
+  {
+    "id": "9",
+    "name": "Kokni Kanteen",
+    "taluka":"Bardez",
+    "desc": "Kokni Kanteen is a family restaurants at panjim, famous for goan fish thali and fish cuisine.",
+    "images": ["images/restaurants/Kokni_Kanteen.jpg"],
+    "location":"https://www.google.com/maps/dir/15.5538051,73.7647483/Kokni+Kanteen/@15.5245712,73.7664283,13z/data=!3m1!4b1!4m9!4m8!1m1!4e1!1m5!1m1!1s0x3bbfc08efc3d27b7:0xbe7c3ef9f3eb1a76!2m2!1d73.8269911!2d15.4966463",
+    "status": "1"
+  },
+  {
+    "id": "10",
+    "name": "Florentine Goa",
+    "taluka":"Bardez",
+    "desc": "Florentine Goa is a family bar and restaurants at saligoa, goa. Famous for its food expecially Chicken Cafreal.",
+    "images": ["images/restaurants/florentine-goa.jpg"],
+    "location":"https://www.google.com/maps/dir/15.493586,73.8301434/Florentine+Goa/@15.5228509,73.7758151,13z/data=!3m1!4b1!4m9!4m8!1m1!4e1!1m5!1m1!1s0x3bbfc1d7ee03501d:0x249217ff579833f1!2m2!1d73.7834929!2d15.5464707",
+    "status": "1"
+  },
+];
