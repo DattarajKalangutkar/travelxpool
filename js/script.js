88255a533dbbb20b3f12e3b4667193cbd6e373fa
Dattaraj <dattaraj@nimapinfotech.com>
2022-03-11 16:06:34 +0530

    added

--- js/script.js
+++ js/script.js
@@ -19,29 +19,29 @@ var swiper = new Swiper(".home-slider", {
         prevEl: ".swiper-button-prev"
     }
 }),
-// swiper = new Swiper(".product-slider", {
-//     loop: !0,
-//     grabCursor: !0,
-//     spaceBetween: 20,
-//     navigation: {
-//         nextEl: ".swiper-button-next",
-//         prevEl: ".swiper-button-prev"
-//     },
-//     breakpoints: {
-//         0: {
-//             slidesPerView: 1
-//         },
-//         640: {
-//             slidesPerView: 2
-//         },
-//         768: {
-//             slidesPerView: 3
-//         },
-//         1024: {
-//             slidesPerView: 4
-//         }
-//     },
-// }),
+swiper = new Swiper(".product-slider", {
+    loop: !0,
+    grabCursor: !0,
+    spaceBetween: 20,
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev"
+    },
+    breakpoints: {
+        0: {
+            slidesPerView: 1
+        },
+        640: {
+            slidesPerView: 2
+        },
+        768: {
+            slidesPerView: 3
+        },
+        1024: {
+            slidesPerView: 4
+        }
+    },
+}),
 swiper = new Swiper(".blogs-slider", {
     loop: !0,
     grabCursor: !0,
@@ -69,4 +69,27 @@ swiper = new Swiper(".dailypost1", {
         nextEl: ".swiper-button-next",
         prevEl: ".swiper-button-prev"
     }
+}),
+swiper = new Swiper(".nearbyPlaceSlider", {
+    loop: !0,
+    grabCursor: !0,
+    spaceBetween: 10,
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev"
+    },
+    breakpoints: {
+        0: {
+            slidesPerView: 1
+        },
+        640: {
+            slidesPerView: 2
+        },
+        768: {
+            slidesPerView: 3
+        },
+        1024: {
+            slidesPerView: 4
+        }
+    },
 });
