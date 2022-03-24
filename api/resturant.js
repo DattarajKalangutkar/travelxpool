397c04ed4805c84a983912d5e84067009d590e19
Rishab <reshab.infipre@gmail.com>
2022-03-24 18:06:30 +0530

    new data added

--- api/resturant.js
+++ api/resturant.js
@@ -2,8 +2,8 @@ var resturantdata = [
   {
     "id": "1",
     "name": "Starlight",
-    "taluka":"",
-    "desc": "Starlight is one of most famous restaurants in south goa at Assolna village. This restaurants is famous for the seafood.",
+    "taluka":"Salcette",
+    "desc": "Starlight is one of the most famous restaurants in south goa at Assolna village situated at the bank of the River. One can enjoy the food with a cool view of the riverside. This place is around 27km from Margoa city on the way to Cabo de Rama beach. One can visit this place at least once to enjoy the delicious seafood and many more. This restaurant is famous for its seafood. You will get all the variety of fish dishes here.",
     "images": ["images/restaurants/starlight.jpg"],
     "location":"https://www.google.com/maps/dir//star+light+assolna/data=!4m6!4m5!1m1!4e2!1m2!1m1!1s0x3bbe4cc6c78ba1dd:0x8b8ff44b279e2ba5?sa=X&ved=2ahUKEwi824ek34P2AhUisFYBHZDwAZkQ9Rd6BAgsEAQ",
     "status": "1"
@@ -11,8 +11,8 @@ var resturantdata = [
   {
     "id": "2",
     "name": "Ritz Classic Panjim",
-    "taluka":"",
-    "desc": "Ritz Classic Panjim is another famous restaurants of goa. It is one of the famous family restaurants in goa.",
+    "taluka":"Tiswadi",
+    "desc": "Ritz Classic Panjim is another famous restaurant in goa. It is a family bar and restaurant with a variety of food delicious. This is an ideal restaurant to visit with your family to enjoy food. The Goa fish thali is an attraction among the locals and a must-try for tourists. The service and ambiguity provided are very good.",
     "images": ["images/restaurants/ritz.jpg"],
     "location":"https://www.google.com/maps/dir//Ritz+Classic+Panjim/data=!4m6!4m5!1m1!4e2!1m2!1m1!1s0x3bbfc0892b7aac1b:0x711b75fe40723402?sa=X&ved=2ahUKEwia9uyu34P2AhXeqFYBHWaTCdIQ9Rd6BAgqEAQ",
     "status": "1"
@@ -24,13 +24,13 @@ var resturantdata = [
     "desc": "Antique Mardol is family restaurants at verna, south goa. It is known for its antique goan foods",
     "images": ["images/restaurants/antique_mardol.jpg"],
     "location":"https://www.google.com/maps/dir//Royal+Treat+Marcela/data=!4m6!4m5!1m1!4e2!1m2!1m1!1s0x3bbfbdb56f247b59:0xa15567f27f788b46?sa=X&ved=2ahUKEwi84ZzK34P2AhXR4jgGHXBOB1gQ9Rd6BAgPEAQ",
-    "status": "1"
+    "status": "0"
   },
   {
     "id": "4",
     "name": "Royal Treat Marcela",
-    "taluka":"",
-    "desc": "Royal Treat is a family restaurants at marcela, Goa. It is one of the underated but famous restaurants amongs local.",
+    "taluka":"Ponda",
+    "desc": "Royal Treat is a family restaurant at Marcela, Goa. It is one of the underrated but famous restaurants in the area and amongst regular visiting tourists in the nearby places. The Goan fish thali is one the famous here and must try. Foods are very delicious and get in good quantity. The rate is very reasonable and the service is also very good. Must visit if you’re in nearby to this place.",
     "images": ["images/restaurants/royal_treat_restaurants.jfif"],
     "location":"https://www.google.com/maps/dir//Tambdi+Surla+Waterfall/@15.421868,74.1962086,12z/data=!3m1!4b1!4m9!4m8!1m1!4e2!1m5!1m1!1s0x3bbf088abfdfa505:0xbea61dfa873fbc3!2m2!1d74.2662491!2d15.421879",
     "status": "1"
@@ -38,8 +38,8 @@ var resturantdata = [
   {
     "id": "5",
     "name": "Darya Dager",
-    "taluka":"",
-    "desc": "Darya Dager is a family bar and restaurants at old goa, famous for its seafoods.",
+    "taluka":"Tiswadi",
+    "desc": "Darya Dager is a family bar and restaurant in old goa, famous for its seafood and other dishes. Many tourists visit this place who comes to visit the old goa churches. The service and ambiance of this place are very good. This restaurant usually gets full on the weekends, so need to check the availability of the table to dine.",
     "images": ["images/restaurants/darya_dager.jpg"],
     "location":"https://www.google.com/maps/dir//Darya+Dager/data=!4m6!4m5!1m1!4e2!1m2!1m1!1s0x3bbfbe897e5f8f7f:0xafe7767be8857468?sa=X&ved=2ahUKEwiw69Ho34P2AhWxwjgGHXEFCOwQ9Rd6BAgNEAQ",
     "status": "1"
@@ -51,7 +51,7 @@ var resturantdata = [
     "desc": "The Fisherman’s Wharf is famous for their signature fusion Goan dishes. There are three outlate at Panaji, Calangute & Cavelossim.",
     "images": ["images/restaurants/the-fisherman-s-wharf.jpg"],
     "location":"https://www.google.com/maps/dir/15.5538051,73.7647483/The+Fisherman's+Wharf,+H.No.+2%2F35B+%26+2%2F35C,+Naika+Waddo,+Calangute,+Goa+403516/@15.5453471,73.7590854,15z/data=!3m1!4b1!4m9!4m8!1m1!4e1!1m5!1m1!1s0x3bbfc12359f2f1d3:0x87cb280e2a74356!2m2!1d73.7664248!2d15.538489",
-    "status": "1"
+    "status": "0"
   },
   {
     "id": "7",
@@ -60,7 +60,7 @@ var resturantdata = [
     "desc": "Martin's Corner is a family bar and restaurants present at Binwaddo, Salcette, Betalbatim.",
     "images": ["images/restaurants/Martins_corner.jpg"],
     "location":"https://www.google.com/maps/dir/15.5538051,73.7647483/Martin's+Corner/@15.4279304,73.7060034,11z/data=!3m1!4b1!4m9!4m8!1m1!4e1!1m5!1m1!1s0x3bbfb6dda67ab363:0x7be42cfd4e9de95!2m2!1d73.9143083!2d15.3037892",
-    "status": "1"
+    "status": "0"
   },
   {
     "id": "8",
@@ -69,7 +69,7 @@ var resturantdata = [
     "desc": "Purple Martini is a bar and resturent at Anjuna Beach, famous for its sunset view seating.",
     "images": ["images/restaurants/Purple_Martini.jpg"],
     "location":"https://www.google.com/maps/dir//Purple+Martini+At+Sunset+Point,+Goa/data=!4m6!4m5!1m1!4e2!1m2!1m1!1s0x3bbfe9835f8284e5:0xb0e2ffdf3e551d62?sa=X&ved=2ahUKEwjX-M-cn5D2AhVvyYsBHTVcAyIQ9Rd6BAhFEAU",
-    "status": "1"
+    "status": "0"
   },
   {
     "id": "9",
@@ -78,7 +78,7 @@ var resturantdata = [
     "desc": "Kokni Kanteen is a family restaurants at panjim, famous for goan fish thali and fish cuisine.",
     "images": ["images/restaurants/Kokni_Kanteen.jpg"],
     "location":"https://www.google.com/maps/dir/15.5538051,73.7647483/Kokni+Kanteen/@15.5245712,73.7664283,13z/data=!3m1!4b1!4m9!4m8!1m1!4e1!1m5!1m1!1s0x3bbfc08efc3d27b7:0xbe7c3ef9f3eb1a76!2m2!1d73.8269911!2d15.4966463",
-    "status": "1"
+    "status": "0"
   },
   {
     "id": "10",
@@ -87,7 +87,7 @@ var resturantdata = [
     "desc": "Florentine Goa is a family bar and restaurants at saligoa, goa. Famous for its food expecially Chicken Cafreal.",
     "images": ["images/restaurants/florentine-goa.jpg"],
     "location":"https://www.google.com/maps/dir/15.493586,73.8301434/Florentine+Goa/@15.5228509,73.7758151,13z/data=!3m1!4b1!4m9!4m8!1m1!4e1!1m5!1m1!1s0x3bbfc1d7ee03501d:0x249217ff579833f1!2m2!1d73.7834929!2d15.5464707",
-    "status": "1"
+    "status": "0"
   },
   {
     "id": "11",
@@ -96,7 +96,7 @@ var resturantdata = [
     "desc": "Copperleaf is a family bar and restaurants famous for its food.Located at Panjim and Porvorim",
     "images": ["images/restaurants/copperleaf.jpg"],
     "location":"https://www.google.com/maps/dir/15.493586,73.8301434/Copperleaf/@15.5162523,73.8136521,14z/data=!3m1!4b1!4m9!4m8!1m1!4e1!1m5!1m1!1s0x3bbfc022a91fe69f:0x6884c44fa19ae2ac!2m2!1d73.8184801!2d15.5388808",
-    "status": "1"
+    "status": "0"
   },
   {
     "id": "12",
@@ -105,6 +105,6 @@ var resturantdata = [
     "desc": "Kuttikar is a family bar and restaurants famous for its sea food.Located near Old goa",
     "images": ["images/restaurants/Kuttikar1.jpg"],
     "location":"https://www.google.com/maps/dir/15.5538051,73.7647483/Kuttikar+Bar+and+Restaurant/@15.5226199,73.7630194,12z/data=!3m1!4b1!4m9!4m8!1m1!4e1!1m5!1m1!1s0x3bbfbee2d3e0af2b:0x61396dc10524d75a!2m2!1d73.8912207!2d15.4975101",
-    "status": "1"
+    "status": "0"
   },
 ];
--- api/waterfall.js
+++ api/waterfall.js
@@ -2,8 +2,8 @@ var waterfalldata = [
   {
     "id": "1",
     "name": "Dudhsagar Waterfall",
-    "taluka":"",
-    "desc": "One of the famous waterfall in the world. Suituated at the heart of mhadei wildlife sanctuary with beautiful landscape.",
+    "taluka":"Dharbandora",
+    "desc": "One of the most famous waterfalls in Asia. Situated at the heart of Bhagwan Mahaveer Sanctuary and Mollem National Park with a beautiful landscape of Western Ghats. It is one of the famous tourist destinations of Goa. One can visit this place with the taxi services provided by the forest authority with the locals. If you like to take a long track then one can reach the waterfall by walking 14km along with the railway track or with the raw road. Taxi services are closed during the rainy season but can reach the place by walking along with railway track.The best time to visit the fall is in the rainy season just to see the waterfall.",
     "images": ["images/waterfalls/dudhsagar.jpg"],
     "location":"https://www.google.com/maps/dir//Dudhsagar+Waterfall/@15.3144266,74.2442668,12z/data=!3m1!4b1!4m9!4m8!1m1!4e2!1m5!1m1!1s0x3bbfa618142b8b43:0xfd9448e7283b0225!2m2!1d74.3143073!2d15.3144375",
     "status": "1"
@@ -11,8 +11,8 @@ var waterfalldata = [
   {
     "id": "2",
     "name": "Kuskem Waterfall",
-    "taluka":"",
-    "desc": "Sutiuated in the south goa. it is the one of the beautiful waterfall with gushing waters flowing past a green forest are so cool and clean that you might want to bathe in them",
+    "taluka":"Canacona",
+    "desc": "Situated in the Cotigao Wildlife Sanctuary in south goa. It is one of the beautiful waterfalls flowing past a beautiful green forest. Best time to visit this place in the mansion season. One needs to track for a few minutes to reach the bottom of the fall. forest department permission is needed to visit this place.The water of the fall is very clean and cold.  One should need to visit this place if want to have the urban side of goa.",
     "images": ["images/waterfalls/kuskem.jpg"],
     "location":"https://www.google.com/maps/dir//Kuskem+Waterfall/data=!4m6!4m5!1m1!4e2!1m2!1m1!1s0x3bbe59c7a1ffd5a1:0x36ffd5dc4fb284dc?sa=X&ved=2ahUKEwjXjOqI4IP2AhW4zDgGHR_IB40Q9Rd6BAgHEAM",
     "status": "1"
@@ -20,8 +20,8 @@ var waterfalldata = [
   {
     "id": "3",
     "name": "Arvalem Waterfalls",
-    "taluka":"",
-    "desc": "One of the famous and most visited water fall in goa. Sutiuated near sanqulim city of goa",
+    "taluka":"Sanquelim",
+    "desc": "One of the famous and most visited waterfalls in goa. Situated near Sanquelim city of goa. It is one of the short waterfalls in height. Near this fall you can visit and take blessings in the Rudreshwar temple.Famous Pandavs (Harvalem Caves) caves are very near to this place. This is an ideal area for school picnics and trips.This is also the spiritual importance place for Hindus as they ago there to perform the 11th-day ritual after the death of the family member.",
     "images": ["images/waterfalls/harvalem.jpg"],
     "location":"https://www.google.com/maps/dir//Arvalem+Waterfalls/data=!4m6!4m5!1m1!4e2!1m2!1m1!1s0x3bbfbd4c54c04655:0x623633fe068aab4b?sa=X&ved=2ahUKEwiL3KWW4IP2AhVaxzgGHf2hAEcQ9Rd6BAgOEAM",
     "status": "1"
@@ -29,8 +29,8 @@ var waterfalldata = [
   {
     "id": "4",
     "name": "Tambdi Surla Waterfall",
-    "taluka":"",
-    "desc": "Another captivating waterfall in Goa situated within the Bhagwan Mahavir Sanctuary, the Tambdi Surla Waterfall is the perfect place to enjoy Goa’s wonderful monsoons.",
+    "taluka":"Dharbandora",
+    "desc": "One of the adventurous waterfalls in Goa is situated within the Bhagwan Mahavir Sanctuary, one needs to track for about 30min to reach this place from the Famous Tambdi Surla Mahadev temple which is a 13th century-old temple. One can take the help of the locals as a guide to reach this place or can join the local tourist who already knows the place. This place is in deep in the forest area and an ideal place to visit to get closer to nature and to spend some time away from the city noise.",
     "images": ["images/waterfalls/tambdisurla.jpg"],
     "location":"https://www.google.com/maps/dir//Tambdi+Surla+Waterfall/@15.421868,74.1962086,12z/data=!3m1!4b1!4m9!4m8!1m1!4e2!1m5!1m1!1s0x3bbf088abfdfa505:0xbea61dfa873fbc3!2m2!1d74.2662491!2d15.421879",
     "status": "1"
@@ -38,8 +38,8 @@ var waterfalldata = [
   {
     "id": "5",
     "name": "Netravali Waterfall",
-    "taluka":"",
-    "desc": "Another waterfall in Goa that is a jewel in its crown, the Netravali Waterfall is tucked away inside the Netravali Wildlife Sanctuary. This waterfall comprises two falls–the Savari and the Mainap.",
+    "taluka":"Sanguem",
+    "desc": "Another waterfall in Goa that is a jewel in its crown, the Netravali Waterfall is Situated inside the Netravali Wildlife Sanctuary. This waterfall comprises two falls–the Savari and the Mainapi. One needs to take permission from the forest department to visit this place. Both waterfalls are far away from the main road and need to tracks for 30 to 40 minutes to reach these waterfalls. These waterfalls are ideal to visit all the months of the year. One can also visit the strawberry farms near these falls and enjoy the fresh strawberry from the farms.",
     "images": ["images/waterfalls/netravali.jpg"],
     "location":"https://www.google.com/maps/dir//Netravali+Waterfall/data=!4m6!4m5!1m1!4e2!1m2!1m1!1s0x3bbef86db74560f5:0xc0d9f1fae04ca3e5?sa=X&ved=2ahUKEwib3I-34IP2AhXuxzgGHflpCTgQ9Rd6BAgLEAM",
     "status": "1"
