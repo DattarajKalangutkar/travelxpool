ace246badc1c8a8ceecf332b6080b32f2a3fbe95
Dattaraj <dattaraj@nimapinfotech.com>
2022-03-26 23:44:25 +0530

    added

---
+++ api/tales.js
@@ -0,0 +1,14 @@
+var tales = [
+	{
+		"id":"1",
+		"name":"Godi Mundi",
+		"url":"godimundi.html",
+		"status":"1"
+	},
+	{
+		"id":"2",
+		"name":"Shigmo",
+		"url":"shigmo.html",
+		"status":"1"
+	}
+].filter((res)=>(res.status == "1"));
