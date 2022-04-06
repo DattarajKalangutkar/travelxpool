f01cb5e4b861150e5a63b5b983745c27af00c301
Rishab <reshab.infipre@gmail.com>
2022-04-06 16:56:49 +0530

    changes n historic places section

--- api/historicplaces.js
+++ api/historicplaces.js
@@ -4,7 +4,11 @@ var historicdata = [
     "name": "Tambdi Surla Temple",
     "taluka":"Dharbandora",
     "desc": "Tambdi Surla temple was built by the Kadamba dynasty king in the 12th-century and is dedicated to Lord Shiva. It was built from basalt stone carved by skillful craftsmen. It is believed to be the only representative of Kadamba architecture built from basalt stone preserved in Goa.  It is a Monument of National Importance protected by ASI.",
-	"images": ["images/historic_places/tambditemple.jpeg"],
+	"images": ["images/historic_places/tambditemple.jpeg",
+	    "images/historic_places/tambdi_surla_temple2.jpeg",
+		"images/historic_places/tambdi_surla_temple3.jpeg",
+		"images/historic_places/tambdi_surla_temple4.jpeg",
+	],
     "full_desc":[
       "Tambdi Surla temple was built by the Kadamba dynasty king in the 12th-century and is dedicated to Lord Shiva. It was built from basalt stone carved by skillful craftsmen. It is believed to be the only representative of Kadamba architecture built from basalt stone preserved in Goa.  It is a Monument of National Importance protected by ASI.",
 	  "The temple has survived the destruction of Mughals and Portuguese invaders.  The temple is near a small village called Tambdi Surla of Dharbandora taluka, in the northeast region of the Bhagwan Mahaveer Sanctuary and Mollem National Park. The small river flows nearby to the temple which visiting people use to clean themselves before entering the temple.",
@@ -43,12 +47,15 @@ var historicdata = [
     "name": "Shri Shantadurga Temple",
     "taluka":"Ponda",
     "desc": "Shri Shantadurga Saunsthan is a temple located at the foothill of Kavalem village in Ponda Taluka, Goa, India.",
-    "images": ["images/historic_places/Shantadurga_Temple_Kavlem_Main.jpg"],
+    "images": ["images/historic_places/Shri_Shantadurga_Temple_ponda_1.jpeg",
+	  "images/historic_places/Shri_Shantadurga_Temple_ponda_2.jpeg",
+      "images/historic_places/Shri_Shantadurga_Temple_ponda_3.jpeg",
+      "images/historic_places/Shri_Shantadurga_Temple_ponda_4.jpeg",],
     "full_desc":[
       "One of the famous temples of goa Shri Shantadurga Temple is situated at Kavalem village of  Ponda Taluka, Goa. The Temple is dedicated to Goddess  Shantadurga, who mediates between Vishnu and Shiva, which is shown as holding two serpents, one in each hand, representing Vishnu and Shiva. legends as it says that once a battle between Shiva and Vishnu took place and was so fierce that the god Brahma prayed to Goddess Parvati to intervene, which she did in the form of Shantadurga. Shantadurga placed Vishnu on her right-hand side and Shiva on her left-hand side to settle the fight.",
-      "The goddess was transferred from Quelossim (Keloshi) in Salcette to Kavalem when the original temple was destroyed by the Portuguese in 1564. Initially, a small shrine with blocks of laterite mud was built and the deity was installed here and worship was continued thereafter. A later foundation stone was laid in 1730  to convert a small shrine into a beautiful time during the reign of Maratha ruler Chattrapati Shahu. The temple was completed in 1738. In a recent year, this temple was renovated in 1966.",
-      "The temple is surrounded by the natural beauty of the Kavalem village, Temple compound has a huge water tank, a Deepastambha(Light tower), and agrashalas (guest houses). This temple is visited by many people or devotees all over India as well as outside India."
-    ],
+      "The goddess was transferred from Quelossim (Keloshi) in Salcette to Kavalem when the original temple was destroyed by the Portuguese in 1564. Initially, a small shrine with blocks of mud was built and the deity was installed here and worship was continued thereafter. A later foundation stone was laid in 1730  to convert a small shrine into a beautiful time during the reign of Maratha ruler Chattrapati Shahu. The temple was completed in 1738. In a recent year, this temple was renovated in 1966.",
+      "The temple is surrounded by the natural beauty of the Kavalem village, Temple compound has a huge water tank, a Deepastambha(Light tower), and agrashalas (guest houses). This temple is visited by many people or devotees all over India as well as outside India.",
+	  ],
     "location":"https://www.google.com/maps/dir//Shree+Shantadurga+Mandir,+Kapileswari+-+Kavlem+Rd,+Donshiwado,+Ponda,+Goa+403401/data=!4m6!4m5!1m1!4e2!1m2!1m1!1s0x3bbfc056dbb98bad:0x7062e4b8d2679e31?sa=X&ved=2ahUKEwjTmIX03oP2AhVCsFYBHat4AIsQ48ADegQIHRAo",
     "status": "1"
   },
@@ -57,12 +64,12 @@ var historicdata = [
     "name": "Safa Masjid Ponda",
     "taluka":"Ponda",
     "desc": "Safa Mosque or Safa Shahouri Mosque is located at Ponda, Goa. It is a 16th century Islamic Monument consisting of garden and fountains. It is protected Monument by an ASI of National Importance in Goa.",
-    "images": ["images/historic_places/SafaMasjidPondaGoa.jpg"],
+    "images": ["images/historic_places/Safa_masjid_ponda1.jpeg"],
     "full_desc":[
       "Safa Mosque also known as Safa Shahouri Mosque is a mosque in Ponda, Goa, India. It is a single chamber mosque with a terracotta tile roof built with the Indo-Islamic style of architecture.  The mosque is surrounded by a complex with gardens and a laterite stone masonry tank which is the like main attraction of this place. It is one of the ASI-protected monuments of National Importance in Goa.",
       "According to the historian, It is a sixteenth-century Islamic Monument built around the year 1560 by the Bijapur ruler Ibrahim Adil Shah I or even before. It is one of the oldest and most prominent Islamic structures in goa. The mosque was damaged, burned, and was left in ruins by the Portuguese, During its rule over Goa. Mosque was partially reconstructed in the 1980s by the goa govt.",
       "This mosque is visible from the main national highway and 1km from both ponda city bustand. Anyone can reach this play by any means of possible transport mode. The best time to visit this place is in the evening to sit quietly and enjoy the evening. There are no entry charges applicable to visit this place. anyone can visit this as they can visit another mosque."
-    ],
+	  ],
     "location":"https://www.google.com/maps/dir//Safa+Masjid+Ponda/data=!4m6!4m5!1m1!4e2!1m2!1m1!1s0x3bbfbaec86860f71:0xdca460046435cfb8?sa=X&ved=2ahUKEwjFtISC34P2AhU7rlYBHdXfDfwQ9Rd6BAglEAU",
     "status": "1"
   },
@@ -78,7 +85,7 @@ var historicdata = [
       "People can visit this place from 9:30 am to 5:00 Pm every day except on Monday when the fort is closed to the general public. An entry fee of 50 rupees per person is applicable to visit the fort."
     ],
     "location":"https://www.google.com/maps/dir//Reis+Magos+Fort/data=!4m6!4m5!1m1!4e2!1m2!1m1!1s0x3bbfc0f0dc93e161:0x6b80a342a7a4c04b?sa=X&ved=2ahUKEwiv-c6L34P2AhUrr1YBHSkwDk8Q9Rd6BAgfEAQ",
-    "status": "1"
+    "status": "0"
   },
   {
     "id": "6",
@@ -111,6 +118,6 @@ var historicdata = [
 	  " Chaitra Poornima, Anguish Satrap, the 8 days Kartik Poornima, and Mahashivratri are the festivals that are celebrated at this temple. The time of the temple visit is 5:30 am to 8:30 pm.Five-storied Deepstambha and the historic old small water pond are the attractions around the temple. One unique aspect of this temple is that you can see the reflection of the idol of Lord Nagesh along with lighted lamps in the inner sanctum if you standing at a certain point around the water pond. ",
 	  ],
     "location":"https://www.google.com/maps/dir/15.4900686,73.7830858/Nagueshi+Temple/@15.4602842,73.7387284,11z/data=!3m1!4b1!4m9!4m8!1m1!4e1!1m5!1m1!1s0x3bbfbaf5f1bc9df5:0x38f7fd83e319208a!2m2!1d73.9837414!2d15.4074688",
-    "status": "1"
+    "status": "0"
   },
 ];
