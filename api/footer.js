86379b28b5e81e95f353c02e38a054c18b7e26c3
Dattaraj <dattaraj@nimapinfotech.com>
2022-04-11 13:18:21 +0530

    added

--- api/footer.js
+++ api/footer.js
@@ -1,26 +1,56 @@
 var footerData = [
 	{
-		"id":"1",
 		"name":"Home",
 		"url":"https://www.travelxpool.com/index.html",
 		"status":"1"
 	},
 	{
-		"id":"2",
 		"name":"About Us",
 		"url":"https://www.travelxpool.com/aboutus.html",
 		"status":"1"
 	},
 	{
-		"id":"3",
 		"name":"Privacy Policy",
-		"url":"https://www.travelxpool.com/privacy_policy.html.html",
+		"url":"https://www.travelxpool.com/privacy_policy.html",
 		"status":"1"
 	},
 	{
-		"id":"4",
 		"name":"Contact",
 		"url":"https://www.travelxpool.com/contact.html",
 		"status":"1"
 	}
 ];
+
+document.getElementById("footerlinks").innerHTML = getFooterLink(footerData);
+
+var headerData = [
+	{
+		"name":"Home",
+		"url":"https://www.travelxpool.com/index.html",
+		"status":"1"
+	},
+	{
+		"name":"About Us",
+		"url":"https://www.travelxpool.com/aboutus.html",
+		"status":"1"
+	},
+	{
+		"name":"Explore",
+		"url":"https://www.travelxpool.com/index.html#packages",
+		"status":"1"
+	},
+	{
+		"name":"IPL 2022",
+		"url":"https://www.travelxpool.com/ipl.html",
+		"status":"1"
+	},
+	{
+		"name":"Contact",
+		"url":"https://www.travelxpool.com/contact.html",
+		"status":"1"
+	},
+];
+
+document.getElementById("headerlink").innerHTML = getFooterLink(headerData);
+
+
