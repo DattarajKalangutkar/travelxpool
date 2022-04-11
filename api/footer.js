5a42a4e2c467387e0c17457adf8b539da7308b54
Dattaraj <dattaraj@nimapinfotech.com>
2022-04-11 12:55:53 +0530

    added

---
+++ api/footer.js
@@ -0,0 +1,26 @@
+var footerData = [
+	{
+		"id":"1",
+		"name":"Home",
+		"url":"https://www.travelxpool.com/index.html",
+		"status":"1"
+	},
+	{
+		"id":"2",
+		"name":"About Us",
+		"url":"https://www.travelxpool.com/aboutus.html",
+		"status":"1"
+	},
+	{
+		"id":"3",
+		"name":"Privacy Policy",
+		"url":"https://www.travelxpool.com/privacy_policy.html.html",
+		"status":"1"
+	},
+	{
+		"id":"4",
+		"name":"Contact",
+		"url":"https://www.travelxpool.com/contact.html",
+		"status":"1"
+	}
+];
--- js/common_function.js
+++ js/common_function.js
@@ -8,3 +8,13 @@ function displayShort(data,descNumber,url)
     str += " ... <a href='"+url+"' title='read more'>read more</a>";
     return str;
 } 
+
+function getFooterLink(data)
+{
+    var str = '';
+    for(let i=0;i<data.length;i++)
+    {
+        str += '<a href="'+data[i].url+'" title="'+data[i].name+'">'+data[i].name+'</a>';
+    }
+    return str;
+}
